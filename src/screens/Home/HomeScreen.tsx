ff2867b93892ede9c2e65af6dcc67f0ec8e590b0
Noa Yarin <yarin066@gmail.com>
2026-02-19 15:20:07 +0200

    Use of slice

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,25 +1,22 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { LanguageToggle } from '../../components/LanguageToggle';
+import { useTranslation } from '../../hooks/use-translation';
 import type { SupportedLanguage } from '../../locales/i18n';
-import { setLanguage } from '../../store/settingsSlice';
-import { styles } from './HomeScreen.styles';
+import { setLanguage } from '../../redux/slices/language-slice';
+import { styles } from './home.styles';
 
 export const HomeScreen: React.FC = () => {
-  const { t } = useTranslation();
+  const { t, currentLanguage } = useTranslation();
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const currentLang = useSelector(
-    (state: any) => state.settings.language as SupportedLanguage,
-  );
-
   const toggleLanguage = () => {
-    const nextLang: SupportedLanguage = currentLang === 'en' ? 'he' : 'en';
+    const nextLang: SupportedLanguage =
+      currentLanguage === 'en' ? 'he' : 'en';
     dispatch(setLanguage(nextLang));
   };
 
@@ -37,7 +34,7 @@ export const HomeScreen: React.FC = () => {
 
       <View style={styles.buttonRow}>
         <LanguageToggle
-          currentLanguage={currentLang}
+          currentLanguage={currentLanguage}
           onToggle={toggleLanguage}
         />
 
@@ -46,7 +43,7 @@ export const HomeScreen: React.FC = () => {
           onPress={handleStartOnboarding}
         >
           <Text style={styles.buttonText}>
-            {t('common.start', 'Start')}
+            {t('onboarding.startOnboarding')}
           </Text>
         </TouchableOpacity>
       </View>
