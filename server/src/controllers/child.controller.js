0d4beede251b48353ee5d18cfd80ba871075dfa2
Noa Yarin <yarin066@gmail.com>
2026-03-01 15:54:55 +0200

    Moved child logic to its file

---
+++ server/src/controllers/child.controller.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { addChild, getMyChild, setChildActive } from "../services/parent.service.js";
+import { Common as CommonErrors } from "../constants/errors.js";
+
+export async function addChildController(req, res, next) {
+  try {
+    const parentId = req.user.parentId;
+    const data = await addChild(parentId, req.body);
+    res.status(201).json({ ok: true, data });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getMyChildController(req, res, next) {
+  try {
+    const parentId = req.user.parentId;
+    const includeInactive = req.query?.includeInactive === "true";
+    const data = await getMyChild(parentId, { includeInactive });
+    res.json({ ok: true, data });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function setChildActiveController(req, res, next) {
+  try {
+    const parentId = req.user.parentId;
+    const { childId } = req.params;
+    const { isActive } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(childId)) {
+      const e = CommonErrors.VALIDATION_CHILD_ID;
+      return res.status(e.status).json({ ok: false, error: { code: e.code, message: e.message } });
+    }
+    if (typeof isActive !== "boolean") {
+      const e = CommonErrors.VALIDATION_IS_ACTIVE;
+      return res.status(e.status).json({ ok: false, error: { code: e.code, message: e.message } });
+    }
+
+    const data = await setChildActive(parentId, childId, isActive);
+    res.json({ ok: true, data });
+  } catch (err) {
+    next(err);
+  }
+}
