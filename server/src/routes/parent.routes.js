401730c40069c4c7e0f3d05bc2c4e0dc031a4c98
Noa Yarin <yarin066@gmail.com>
2026-03-01 15:58:42 +0200

    Fixed routes

---
+++ server/src/routes/parent.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { authJwt } from "../middlewares/authJwt.js";
+import {
+  addChildController,
+  getMyChildController,
+  setChildActiveController,
+} from "../controllers/child.controller.js";
+import { requireParent } from "../middlewares/requireParent.js";
+
+const router = Router();
+
+// api/v1/parents/add/child
+router.post("/add/child", authJwt, requireParent, addChildController);
+// api/v1/parents/get/child
+router.get("/get/child", authJwt, requireParent, getMyChildController);
+// api/v1/parents/set/child/:childId/active  
+router.patch("/set/child/:childId/active", authJwt, requireParent, setChildActiveController);
+export default router;
