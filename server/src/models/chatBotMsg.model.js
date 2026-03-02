33adf630c954a16ad1d75b9b5758defa7ba55e26
Shiri667377 <sw667377@gmail.com>
2026-03-02 20:54:12 +0200

    fixed chat models fields to updated ones

--- server/src/models/chatBotMsg.model.js
+++ server/src/models/chatBotMsg.model.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
+import { SenderRole } from "../constants/role.js";
 
 export const ChatBotMsgSchema = new mongoose.Schema({
-    senderId: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
+
+    parentId: { type: mongoose.Schema.Types.ObjectId,ref: "Parent", required: true },
+    childId: { type: mongoose.Schema.Types.ObjectId, required: true }, 
     templateId: { type: mongoose.Schema.Types.ObjectId, ref: "ChatTemplate", required: true },
-  });
+     senderRole: {
+      type: String,
+      enum: Object.values(SenderRole),
+      required: true,
+    },
+  }, { timestamps: true });
+
   export default mongoose.model("ChatBotMsg", ChatBotMsgSchema);
--- server/src/models/chatTemplate.model.js
+++ server/src/models/chatTemplate.model.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+import { TargetRole } from "../constants/role.js";
+
 
 export const ChatTemplateSchema = new mongoose.Schema({
     isMenu: { type: Boolean, default: false },
     menuOptionNumber: { type: Number },
     content: { type: String, required: true },
-    category: { type: String }
+    category: { type: String },
+    TargetRole: {
+      type: String,
+      enum: Object.values(TargetRole),
+      required: true,
+      default: TargetRole.BOTH,
+    },
   }, { timestamps: true });
 export default mongoose.model("ChatTemplate", ChatTemplateSchema);
