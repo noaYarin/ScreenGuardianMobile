66e1aae2a90b70e5f04e24d337c0a7bd1b7d7935
Shiri667377 <sw667377@gmail.com>
2026-03-17 19:44:12 +0200

    connect AddChild screen to backend using Redux (addChildThunk), add children slice, loading and error handling

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import languageReducer from '../../redux/slices/language-slice';
 import authReducer from '../../redux/slices/auth-slice';
+import childrenReducer from "../slices/children-slice";
 
 export const store = configureStore({
   reducer: {
     language: languageReducer,
     auth: authReducer,
+    children: childrenReducer,
   },
 });
 
