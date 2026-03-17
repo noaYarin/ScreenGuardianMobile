acbac09885891e545059a4aacde3ea1fa5f41074
Shiri667377 <sw667377@gmail.com>
2026-03-17 21:32:39 +0200

    fixing child thunks to seprate file

---
+++ client/src/redux/thunks/childrenThunks.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as parentApi from "@/src/api/parent";
+
+type AddChildPayload = {
+  name: string;
+  birthDate: string;
+  gender?: string;
+};
+
+export const addChildThunk = createAsyncThunk<
+  any,
+  AddChildPayload,
+  { rejectValue: string }
+>("children/addChild", async (payload, thunkAPI) => {
+  try {
+    const response = await parentApi.addChild(payload);
+    return response.child;
+  } catch (error) {
+    const message =
+      (error as Error)?.message ?? "children.add_failed";
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+export const getMyChildrenThunk = createAsyncThunk<
+  any[],
+  void,
+  { rejectValue: string }
+>("children/getMyChildren", async (_, thunkAPI) => {
+  try {
+    const response = await parentApi.getMyChildren();
+    return response.children;
+  } catch (error) {
+    const message =
+      (error as Error)?.message ?? "children.fetch_failed";
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
