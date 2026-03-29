965a8e49731d068c7fbf76e20fe5d13d0f746fa3
Noa Yarin <yarin066@gmail.com>
2026-03-29 10:47:32 +0300

    Delete logs

--- server/src/controllers/device.controller.js
+++ server/src/controllers/device.controller.js
@@ -223,11 +223,6 @@ export async function getDeviceCurrentStatusForChildController(req, res, next) {
 
 export async function updateDeviceLocationController(req, res, next) {
     try {
-      const { deviceId } = req.params;
-      const { location } = req.body;
-      const { parentId, childId } = req.user; 
-      console.log("📥 Incoming Device ID:", req.params.deviceId);
-      console.log("👤 User from Token:", req.user);
       const data = await updateDeviceLocation(deviceId, location, parentId, childId);
   
       res.status(200).json({ ok: true, data });
