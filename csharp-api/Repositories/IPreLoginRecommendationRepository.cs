33feb58ce2011ae091a4795cd36a610050114e9f
Shiri667377 <sw667377@gmail.com>
2026-04-05 01:57:16 +0300

    Add C# API and update env config

---
+++ csharp-api/Repositories/IPreLoginRecommendationRepository.cs
@@ -0,0 +1,9 @@
+﻿using ScreenGuardianAPI.Models;
+
+namespace ScreenGuardianAPI.Repositories
+{
+    public interface IPreLoginRecommendationRepository
+    {
+        PreLoginRecommendation GetRecommendationByAge(int age);
+    }
+}
